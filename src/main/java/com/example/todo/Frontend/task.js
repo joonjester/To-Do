6d3a6dd7f8ac0e381f884c93552bbec3fbe36630
Hyunjoon Choi <hyunjoonchoi04@gmail.com>
2024-08-17 23:09:22 +0200

    button that copies the text into the input and icon

--- src/main/java/com/example/todo/Frontend/task.js
+++ src/main/java/com/example/todo/Frontend/task.js
@@ -36,7 +36,7 @@ document.getElementById('task_input').addEventListener('keydown', async function
         let parent_id = null;
 
         if (check_for_parent(input_value)) {
-            const end_symbol = input_value.search("<");
+            const end_symbol = input_value.search(":");
             parent_id = await link_parent(input_value, end_symbol);
             input_value = task_for_parent(input_value, end_symbol);
         }
@@ -69,14 +69,15 @@ document.getElementById('task_input').addEventListener('keydown', async function
 
 function create_task(data, parent_id) {
     const li = document.createElement('li');
-    let task_text = document.createElement('div')
-    const button_layout = document.createElement('div')
+    let task_text = document.createElement('div');
+    const button_layout = document.createElement('div');
+    const create_sub_task = document.createElement('i');
     const del_icon = document.createElement('i');
     const edit_icon = document.createElement('i');
     const ul = document.createElement('ul');
 
     //ID for Sub-List of a Task
-    ul.id = 'sub_task_id'+data.id;
+    ul.id = 'sub_task_id' + data.id;
 
     //Create List Element
     task_text.innerText = data.task;
@@ -86,6 +87,12 @@ function create_task(data, parent_id) {
         toggle_task(data.id);
         this.classList.toggle('task')
     };
+
+    create_sub_task.className = 'fa-regular fa-copy';
+    create_sub_task.onclick = function () {
+        document.getElementById('task_input').value = task_text.innerText + ': ';
+        document.getElementById('task_input').focus();
+    }
 
     //Create Button for Edit
     edit_icon.className = 'fa-regular fa-pen-to-square';
@@ -112,6 +119,7 @@ function create_task(data, parent_id) {
 
     // Adding the element to the task
     button_layout.id = 'button_layout';
+    button_layout.appendChild(create_sub_task);
     button_layout.appendChild(edit_icon);
     button_layout.appendChild(del_icon);
     li.appendChild(task_text);
@@ -119,7 +127,7 @@ function create_task(data, parent_id) {
 
     // Deciding which UL it goes to
     if (parent_id !== null) {
-        document.getElementById('sub_task_id'+ parent_id).appendChild(li);
+        document.getElementById('sub_task_id' + parent_id).appendChild(li);
     } else {
         document.getElementById('tasks_list').appendChild(li);
         document.getElementById('tasks_list').appendChild(ul);
@@ -172,8 +180,7 @@ function update_status(id, status) {
 }
 
 function link_parent(text, end_symbol){
-    let parent_task = text.substring(1, end_symbol);
-    console.log(parent_task)
+    let parent_task = text.substring(0, end_symbol);
     return fetch('http://localhost:8080/task/parent_id?parent_task='+ parent_task, {
         method: 'GET'
     })
@@ -249,9 +256,13 @@ function play_audio() {
 }
 
 function check_for_parent(text){
-    let first_char = Array.from(text)[0];
-    return first_char === ">";
-}
+    for (let char of text) {
+        if (char === ":") {
+            return true;
+        }
+    }
+    return false;
+}p
 
 function task_for_parent(text, end_symbol) {
     return text.substring(end_symbol + 2);
